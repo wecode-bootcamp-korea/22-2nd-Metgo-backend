7e48deb7483500bc4b4b25b27f113829ddf3f540
showmethr23 <62336688+showmethr23@users.noreply.github.com>
2021-07-29 17:15:35 +0900

    ADD: Test.py

--- masters/urls.py
+++ masters/urls.py
@@ -1,7 +1,9 @@
 from django.urls import path
 
-from masters.views import MasterSignupView
+from masters.views import MasterSignupView, MasterSigninView, MasterKakaoSigninView
 
 urlpatterns = [
     path('/signup', MasterSignupView.as_view()),
+    path('/signin', MasterSigninView.as_view()),
+    path('/kakao/signin', MasterKakaoSigninView.as_view()),
 ]
