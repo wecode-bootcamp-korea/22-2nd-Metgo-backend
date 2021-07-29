bb5dc4420772e301bf6a9b7e2ea2070904773872
ty <kim@tyui-MacBookAir.local>
2021-07-29 15:12:36 +0900

    MODIFIED: quotations

--- quotations/models.py
+++ quotations/models.py
@@ -1,14 +1,20 @@
-from django.db.models.fields         import BooleanField, DecimalField
+from django.db.models.enums          import TextChoices
+from django.db.models.fields         import CharField, DecimalField
 from django.db.models.deletion       import CASCADE
 from django.db.models.fields.related import ForeignKey
 
 from core.models import TimeStampModel
 
 class Quotation(TimeStampModel):
-    application_master = ForeignKey('applications.ApplicationMaster', on_delete=CASCADE)
-    price              = DecimalField(max_digits=10, decimal_places=2, null=True)
-    is_completed       = BooleanField(null=True)
-    master             = ForeignKey('masters.Master', on_delete=CASCADE, related_name='hired_master')
+    class Status(TextChoices):
+        WAITING  = 'waiting'
+        ACCEPTED = 'accepted'
+        DENIED   = 'denied'
+
+    user           = ForeignKey('users.User', on_delete=CASCADE)
+    price          = DecimalField(max_digits=10, decimal_places=2, null=True)
+    status         = CharField(max_length=10, choices=Status.choices, null=True)
+    master_service = ForeignKey('services.MasterService', on_delete=CASCADE, related_name='hired_master')
 
     class Meta:
         db_table = 'quotations'
